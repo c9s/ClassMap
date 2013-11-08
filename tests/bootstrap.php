a988b27e524c1f71ca27a6aacde2073f52b73186
c9s <cornelius.howl@gmail.com>
2013-11-08 17:14:52 +0800

    Update phpunit bootstrap script

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,2 @@
 <?php
-require 'PHPUnit/TestMore.php';
-require 'Universal/ClassLoader/BasePathClassLoader.php';
-$loader = new \Universal\ClassLoader\BasePathClassLoader( 
-    array('src','vendor/pear' )
-);
-$loader->register();
+require 'vendor/autoload.php';
